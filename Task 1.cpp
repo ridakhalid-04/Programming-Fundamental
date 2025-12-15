d8b8d75ac1db86dad6702845e6f1fe69e1c9a4af
ridakhalid-04 <ridakhalid441@gmail.com>
2025-12-15 20:53:24 +0500

    Add files via upload

---
+++ Task 1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Welcome to the C++ Lab! " << endl;
+	cout<< "You will learn basic output today. " << endl;
+	cout<< "Let's learn with simple messages. " << endl;
+	return 0;
+}
