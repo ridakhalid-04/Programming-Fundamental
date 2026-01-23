4f72eb5fdfe533c0067007b0bea48511a1b0c6b9
ridakhalid-04 <ridakhalid441@gmail.com>
2026-01-23 08:59:53 +0500

    Add files via upload
    Tasks of Lab 1.

---
+++ Lab 1.Task 3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Item\tPrice\tQuality\n";
+	cout << "Pen\t$1\t10\n";
+	cout << "Pencil\t$2\t5\n";
+	cout << "Eraser\t$0.5\t20\n";
+	return 0;
+}
